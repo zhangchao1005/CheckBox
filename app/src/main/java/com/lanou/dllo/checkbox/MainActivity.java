723b4e699d4812c38eb6f51f4e9f06793cd83e9f
dllo <1917143414@qq.com>
2016-03-28 16:12:54 +0800

    第一次提交

---
+++ app/src/main/java/com/lanou/dllo/checkbox/MainActivity.java
@@ -0,0 +1,63 @@
+package com.lanou.dllo.checkbox;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+public class MainActivity extends Activity {
+    private ListView listView; //listview控件
+    private ArrayList<String> contentStrings = new ArrayList<String>(); //数据源
+    private ListViewAdpter adapter;  //适配器
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
+        initData(); //初始化数据
+        setListener();  //绑定事件
+    }
+    private void initData() {
+        contentStrings.add("第1项");
+        contentStrings.add("第2项");
+        contentStrings.add("第3项");
+        contentStrings.add("第4项");
+        contentStrings.add("第5项");
+        contentStrings.add("第6项");
+        contentStrings.add("第7项");
+        contentStrings.add("第8项");
+        contentStrings.add("第9项");
+        contentStrings.add("第10项");
+        contentStrings.add("第11项");
+        contentStrings.add("第12项");
+        contentStrings.add("第13项");
+        adapter=new ListViewAdpter(contentStrings, this);
+        listView.setAdapter(adapter);
+    }
+
+    private void setListener() {
+
+        listView.setOnItemClickListener(new OnItemClickListener() { //点击每项item时候执行
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                Toast.makeText(MainActivity.this, "点击事件，执行你的操作", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+
+}
